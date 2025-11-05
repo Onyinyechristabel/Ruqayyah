6a5af1f21c34ee6132786fc5685e3bf4f1df14e1
Onyinye <onyinye241381@gmail.com>
2025-11-05 20:24:34 +0100

    update

---
+++ pages/workpage.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="workpage">
+    <div class="work_heading">
+      <h2 class="workpage_header">Building user-focused solutions that</h2>
+      <h2 class="workpage_header2">solve problems and scale businesses</h2>
+    </div>
+    <main class="workprojects">
+      <div class="work_work">
+        <div class="work_bg"></div>
+        <img src="/images/a.avif" alt="" class="work_imga" />
+        <div class="work_blackbox">
+          <h3 class="work_blacktext">
+            Fort HCM: Simplifying Hiring for Modern Teams
+          </h3>
+          <div class="work_blackcard">
+            <p class="work_blackcard1">SASS</p>
+            <p class="work_blackcard2">HRM</p>
+          </div>
+        </div>
+      </div>
+      <div class="work_work">
+        <div class="work_bg"></div>
+        <img src="/images/b.avif" alt="" class="work_imga" />
+        <div class="work_blackbox">
+          <h3 class="work_blacktext">
+            Bujeti: Helping Merchants Get Paid Faster
+          </h3>
+          <div class="work_blackcard">
+            <p class="work_blackcard1">SASS</p>
+            <p class="work_blackcard2">FinTech</p>
+          </div>
+        </div>
+      </div>
+      <div class="work_work">
+        <div class="work_bg"></div>
+        <img src="/images/c.avif" alt="" class="work_imga" />
+        <div class="work_blackbox">
+          <h3 class="work_blacktext">Jeroid Mobile App Redesign</h3>
+          <div class="work_blackcard">
+            <p class="work_blackcard1">Cryptocurrency</p>
+            <p class="work_blackcard2">FinTech</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  </section>
+</template>
