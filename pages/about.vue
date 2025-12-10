a7bc83eaeb344dc6e8b27677339693338977a268
Onyinye <onyinye241381@gmail.com>
2025-12-10 10:50:07 +0100

    about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,196 @@
 <template>
   <section class="about">
     <h2 class="about_h2">About Me</h2>
+    <div class="about_spans">
+      <span>I do good designs </span>
+      <span> <img src="/images/ruqa.jpeg" alt="" /></span>
+      <span>Play games</span>
+      <span> <img src="/images/ruqa.jpeg" alt="" /></span>
+      <span>Love Seyi Vibez</span>
+      <span> <img src="/images/ruqa.jpeg" alt="" /></span>
+    </div>
+    <p class="about_p1">
+      I’m Ruqayyah, a product designer passionate about creating user-centered
+      digital experiences that balance creativity with strategy. My experience
+      spans fintech, SaaS, and community-driven products, where I focus on
+      solving real problems and driving impact through design. I also mentor
+      designers and contribute to communities that help others grow in their
+      careers.
+    </p>
+    <p class="about_p2">
+      Beyond design, I’m a gamer who enjoys CODM, and I find inspiration in
+      music especially Seyi Vibez (if you’re not a fan, you’re probably bad
+      vibes 😅). I believe blending creativity with play keeps my perspective
+      fresh and fuels the way I approach design.
+    </p>
+    <div class="about_jpegs">
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+      <img src="/images/ruqa.jpeg" alt="" />
+    </div>
+    <div class="about_shadow"></div>
+    <div class="about_shadow2"></div>
+    <main class="about_explore">
+      <div class="about_explore1">
+        <div class="about_netflix">
+          <img class="netflixlogo" src="/images/ruqa.jpeg" alt="" />
+          <h2>Currently Watching</h2>
+          <p>The Rookie</p>
+          <img class="netfliximg" src="/images/ruqa.jpeg" alt="" />
+        </div>
+        <div class="about_quote">
+          <img src="/images/ruqa.jpeg" alt="" />
+          <h3>Favorite Quote</h3>
+          <h2>Fabiayyi Alai Rabbikuma Tukhatiban</h2>
+          <p>So which of the favors of your Lord would you deny?</p>
+        </div>
+        <div class="about_music">
+          <div class="about_music_pro">
+            <img src="/images/ruqa.jpeg" alt="" />
+            <h2>What I am listening to</h2>
+          </div>
+          <div class="about_spotifys">
+            <div class="about_spotify">
+              <h3>The Good Ol' Days</h3>
+              <div class="about_div">
+                <img src="/images/ruqa.jpeg" alt="" />
+                <img src="/images/ruqa.jpeg" alt="" />
+              </div>
+            </div>
+            <div class="about_spotify">
+              <h3>The Good Ol' Days</h3>
+              <div class="about_div">
+                <img src="/images/ruqa.jpeg" alt="" />
+                <img src="/images/ruqa.jpeg" alt="" />
+              </div>
+            </div>
+            <div class="about_spotify">
+              <h3>The Good Ol' Days</h3>
+              <div class="about_div">
+                <img src="/images/ruqa.jpeg" alt="" />
+                <img src="/images/ruqa.jpeg" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="about_explore2">
+        <div class="about_socials">
+          <h2>Connect with me</h2>
+          <div class="about_ss">
+            <div class="about_social_div">
+              <img src="/images/ruqa.jpeg" alt="" />
+              <p>Instagram</p>
+            </div>
+            <div class="about_social_div">
+              <img src="/images/ruqa.jpeg" alt="" />
+              <p>X (formerly Twitter)</p>
+            </div>
+            <div class="about_social_div">
+              <img src="/images/ruqa.jpeg" alt="" />
+              <p>LinkedIn</p>
+            </div>
+            <div class="about_social_div">
+              <img src="/images/ruqa.jpeg" alt="" />
+              <p>Dribbble</p>
+            </div>
+          </div>
+        </div>
+        <div class="about_books">
+          <div class="about_booktitle">
+            <img src="/images/ruqa.jpeg" alt="" />
+            <div class="about_booktitle_content">
+              <h2>Last Read</h2>
+              <h1>
+                Broken: <br />
+                Not A Halal Love Story
+              </h1>
+            </div>
+          </div>
+          <img class="about_bookimg" src="/images/ruqa.jpeg" alt="" />
+        </div>
+      </div>
+      <div class="about_explore3">
+        <div class="about_cod">
+          <img src="/images/ruqa.jpeg" alt="" />
+          <div class="about_cod_content">
+            <h2>Call of Duty: Mobile</h2>
+            <h1>Grandmaster 1 BR/MP</h1>
+          </div>
+        </div>
+        <img src="/images/ruqa.jpeg" alt="" class="about_cod_img" />
+      </div>
+    </main>
+    <div class="about_experience">
+      <h1>Work experiences</h1>
+      <div class="about_experience_div">
+        <div class="about_work_experience">
+          <h2>Product Designer</h2>
+          <p>Regent MFB</p>
+          <h3>Oct 2024 - Jun 2025</h3>
+        </div>
+        <div class="about_work_experience">
+          <h2>Founding Designer</h2>
+          <p>Fort HCM</p>
+          <h3>Aug 2023</h3>
+        </div>
+        <div class="about_work_experience">
+          <h2>Product Designer</h2>
+          <p>Bujeti</p>
+          <h3>Jun 2022 - Aug 2023</h3>
+        </div>
+        <div class="about_work_experience">
+          <h2>Product Designer</h2>
+          <p>Crenet Techlabs</p>
+          <h3>Mar 2022 - Jul 2022</h3>
+        </div>
+      </div>
+    </div>
+    <div class="about_tools">
+      <h2>My Tools</h2>
+      <div class="about_tools_content">
+        <div class="about_tools_details">
+          <img src="/images/ruqa.jpeg" alt="" />
+          <div class="about_tools_div">
+            <h3>Figma</h3>
+            <p>Product Design</p>
+          </div>
+        </div>
+        <div class="about_tools_details">
+          <img src="/images/ruqa.jpeg" alt="" />
+          <div class="about_tools_div">
+            <h3>Framer</h3>
+            <p>No Code Development</p>
+          </div>
+        </div>
+        <div class="about_tools_details">
+          <img src="/images/ruqa.jpeg" alt="" />
+          <div class="about_tools_div">
+            <h3>Click Up</h3>
+            <p>Product Management</p>
+          </div>
+        </div>
+        <div class="about_tools_details">
+          <img src="/images/ruqa.jpeg" alt="" />
+          <div class="about_tools_div">
+            <h3>Notion</h3>
+            <p>Documentation</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
